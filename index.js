692039c982697ab599e7d827f143490e8ae5da87
HectorGuerrini <hectorumaga@hotmail.com>
2019-01-31 18:12:16 -0200

    ajustes procs e cabecalho

--- index.js
+++ index.js
@@ -23,16 +23,99 @@ console.log(firebirdConfig)
 
 app.get('/', (req, res) => res.send('Hello Delvetro!'))
 app.post('/listaVendas', (req, res) => {
-    firebirdConfig.Execute(`SELECT * FROM vendas WHERE ven_data = '${req.body.data}' ORDER BY VEN_HORA DESC `, (result) => {
-        res.json(result);
-    });
+    firebirdConfig.Execute(`
+        SELECT 
+            a.ven_codigo, ven_data, ven_hora, con_codigo, ven_responsavel, ven_total,
+            ven_status, vei_codigo, c.pro_codigo, pro_nome, pro_categoria, pro_setor, vei_med1 altura,
+            vei_med2 largura, VEI_QTDE metro, VEI_SUBQTDE qtde
+        FROM vendas AS a
+        INNER JOIN vendasitens b ON a.ven_codigo = b.ven_codigo
+        INNER JOIN produtos c ON c.pro_codigo = b.pro_codigo        
+        WHERE 
+            ven_data >= '${req.body.dataMin}' AND ven_data <= '${req.body.dataMax}'
+            ${req.body.cliente ? `AND VEN_RESPONSAVEL LIKE '%${req.body.cliente.toUpperCase()}%'`: '' } 
+        ORDER BY VEN_DATA DESC, VEN_HORA DESC `,
+        (result) => {
+            let jsonRetorno = []
+            result.forEach((el) => {
+                let index = jsonRetorno.findIndex((js) => {
+                    return js.ven_codigo == el.VEN_CODIGO
+                });
+                if (index != -1) {
+                    let json = {
+                        vei_codigo: el.VEI_CODIGO,
+                        pro_codigo: el.PRO_CODIGO,
+                        pro_nome: el.PRO_NOME,
+                        pro_categoria: el.PRO_CATEGORIA,
+                        pro_setor: el.PRO_SETOR,
+                        altura: el.ALTURA,
+                        largura: el.LARGURA,
+                        metro: el.ALTURA*el.LARGURA ? el.METRO : 0,
+                        qtde: el.QTDE
+                    }
+                    jsonRetorno[index].produtos.push(json);
+                } else {
+                    let json = {
+                        ven_codigo: el.VEN_CODIGO,
+                        ven_data: el.VEN_DATA,
+                        ven_hora: el.VEN_HORA,                        
+                        con_codigo: el.CON_CODIGO,
+                        ven_responsavel: el.VEN_RESPONSAVEL,
+                        ven_status: el.VEN_STATUS,
+                        ven_total: el.VEN_TOTAL,
+                        status_pagamento: 'Ñ Pago',
+                        qtde_pago: 0,
+                        produtos: [{
+                            vei_codigo: el.VEI_CODIGO,
+                            pro_codigo: el.PRO_CODIGO,
+                            pro_nome: el.PRO_NOME,
+                            pro_categoria: el.PRO_CATEGORIA,
+                            pro_setor: el.PRO_SETOR,
+                            altura: el.ALTURA,
+                            largura: el.LARGURA,
+                            metro: el.ALTURA*el.LARGURA ? el.METRO : 0,
+                            qtde: el.QTDE
+                        }],
+                        caixa: []
+                    }
+                    jsonRetorno.push(json);
+                }
+
+
+            })
+
+            res.json(jsonRetorno);
+        });
 
 });
 app.post('/listaCaixa', (req, res) => {
-    firebirdConfig.Execute(`SELECT * FROM caixa WHERE cai_categoria = 'VENDA' 
-        AND cai_pagamento = '${req.body.data}' ORDER BY cai_codigo DESC `, (result) => {
+    firebirdConfig.Execute(`
+        SELECT 
+            a.ven_codigo, REPLACE(d.cai_id,'VEN ', '') cai_id, ven_data, ven_hora, cai_pagamento, d.cai_codigo, cai_credito, cai_debito, 
+            cai_forma,cai_categoria 
+        FROM vendas AS a
+        INNER JOIN caixa d on d.cai_categoria IN ('VENDA','SERVICOS') and replace(d.cai_id,'VEN ', '')= a.ven_codigo
+        WHERE 
+            ven_data >= '${req.body.dataMin}' AND ven_data <= '${req.body.dataMax}'
+            ${req.body.cliente ? `AND VEN_RESPONSAVEL LIKE '%${req.body.cliente.toUpperCase()}%'`: '' } 
+        ORDER BY VEN_DATA DESC, VEN_HORA DESC`, 
+        (result) => {
         res.json(result);
     });
 
 });
+app.post('/cabecalho', (req, res) => {
+    firebirdConfig.Execute(`
+        SELECT 1 AS id, 'Pedidos' AS label,sum(ven_total) AS valor FROM vendas
+        WHERE ven_data >= '01-01-2019' AND ven_data <= '01-31-2019'
+        UNION ALL
+        SELECT 2 AS id, 'Faturamento' AS label,sum(cai_credito+cai_debito) AS valor FROM caixa
+        WHERE 
+            cai_pagamento >= '${req.body.dataMin}' AND cai_pagamento <= '${req.body.dataMax}' 
+            AND cai_categoria IN ('VENDA','SERVICOS')            
+    `,
+    (result) => {
+        res.json(result)
+    })
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ procVendas.sql
@@ -0,0 +1,22 @@
+select distinct a.ven_codigo,ven_data,ven_hora,cai_pagamento,d.con_codigo,ven_responsavel,ven_total,
+ven_status,vei_codigo,c.pro_codigo,pro_nome,pro_categoria,pro_setor,vei_med1 altura,vei_med2 largura,VEI_QTDE metro,
+VEI_SUBQTDE qtde,
+d.cai_codigo,cai_credito,cai_debito, cai_forma,cai_categoria
+from vendas as a
+inner join vendasitens b on a.ven_codigo = b.ven_codigo
+inner join produtos c on c.pro_codigo = b.pro_codigo
+inner join caixa d on d.cai_categoria IN ('VENDA','SERVICOS') and replace(d.cai_id,'VEN ', '')= a.ven_codigo
+WHERE ven_data >= '01-31-2019' AND ven_data <= '01-31-2019'
+ORDER BY VEN_DATA DESC, VEN_HORA DESC
+
+select a.ven_codigo,ven_data,ven_hora,d.cai_codigo,cai_credito,cai_debito, cai_forma,cai_categoria from vendas as a
+inner join caixa d on d.cai_categoria IN ('VENDA','SERVICOS') and replace(d.cai_id,'VEN ', '')= a.ven_codigo
+WHERE ven_data >= '01-14-2019' AND ven_data <= '01-14-2019'
+ORDER BY VEN_DATA DESC, VEN_HORA DESC
+
+SELECT 1 as id, 'Pedidos' as label,sum(ven_total) as valor FROM vendas
+where ven_data >= '01-01-2019' and ven_data <= '01-31-2019'
+UNION ALL
+SELECT 2 as id, 'Faturamento' as label,sum(cai_credito+cai_debito) as valor FROM caixa
+where cai_pagamento >= '01-01-2019' and cai_pagamento <= '01-31-2019' and cai_categoria IN ('VENDA','SERVICOS')
+
