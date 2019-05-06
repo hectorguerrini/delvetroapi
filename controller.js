d247ee15eea5bcd2d81c73b09a2a48d9af5345d8
HectorGuerrini <hectorumaga@hotmail.com>
2019-05-06 18:07:23 -0300

    produtos

--- controller.js
+++ controller.js
@@ -148,6 +148,12 @@ function getQuery(tipo, method, args, params) {
                 @ID_CLIENTE = ${params.ID}
                 `;
                 break;
+            case 'produtos':
+                query = `
+                EXEC sp_get_produtos
+                @ID_PRODUTOS = ${params.ID}
+                `;
+                break;
         }
     }
     return query;
@@ -161,6 +167,7 @@ exports.cadastro = function (req, res) {
             console.dir(err);
             return;
         }
+        result = req.params.tipo !== 'produtos' ? result : result[0].produtos
         res.json({
             query: query,
             json: result
