dfbd236283a752dfba264ff87ca6b03398b0937c
HectorGuerrini <hectorumaga@hotmail.com>
2019-11-03 12:54:02 -0300

    import helmet

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 import { Routes } from './routes';
 
 import { corsConfig } from './models/cors-config';
-
+import helmet from "helmet";
 
 
 export default class App {
@@ -20,7 +20,7 @@ export default class App {
         
         const corsopt = new corsConfig();
         this.app.use(json());
-
+        this.app.use(helmet())
         this.app.use(cors(corsopt.options));
 
         this.app.use('/delvetroapi', this.routes.router);
