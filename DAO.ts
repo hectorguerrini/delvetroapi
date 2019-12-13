9f2086c0bdd092a973c6905ab80f2dfcecf91ced
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2019-12-13 10:40:50 -0300

    limpeza de codigo

--- DAO.ts
+++ DAO.ts
@@ -15,23 +15,6 @@ export class DAO {
             }).catch(err => {
                 callback(err, undefined);
             })
-
-        // const pool = new ConnectionPool(this.configDev,(err: any) => {
-        //     if (err) {
-        //         console.error("error connecting: " + err.stack);
-        //         callback(err);
-        //     }
-
-        //     const conn = new Request(pool);
-        //     conn.query(query, function (error, result) {
-        //         if (error) {
-        //             console.dir(error);
-        //             callback(error);                
-        //         }
-        //         callback(error, result)
-        //     });
-
-        // })
     }
     queryDBTable<Entity>(procedure: string,req: Request, callback: (err?: Error, recordset?: IProcedureResult<Entity>) => void){
 
@@ -49,21 +32,6 @@ export class DAO {
                 callback(err, undefined);
             })
 
-        // const pool = new ConnectionPool(this.configDev,(err: any) => {
-        //     if (err) {
-        //         console.error("error connecting: " + err.stack);
-        //         callback(err);
-        //     }
 
-        //     const conn = new Request(pool);
-        //     conn.query(query, function (error, result) {
-        //         if (error) {
-        //             console.dir(error);
-        //             callback(error);                
-        //         }
-        //         callback(error, result)
-        //     });
-
-        // })
     }
 }
